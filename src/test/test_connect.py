26e022d1bbb321760d740f7758574e7ae7481586
raulpsl <raulparedesld@gmail.com>
2025-06-23 18:07:31 -0400

    update: update test and route for workflow

--- src/test/test_connect.py
+++ src/test/test_connect.py
@@ -1,8 +1,8 @@
-from scripts.execute import Ejecutar
-from config.conf import BaseConf
-def test_conectar():
-    conn = Ejecutar().ejecutarConsulta(f"SHOW FULL TABLES FROM {BaseConf.SQL_DB};")
-    if conn: 
-        print("Conectado.")
+# from scripts.execute import Ejecutar
+# from config.conf import BaseConf
+# def test_conectar():
+#     conn = Ejecutar().ejecutarConsulta(f"SHOW FULL TABLES FROM sql5786286;")
+#     if conn: 
+#         print("Conectado.")
 
-test_conectar()
+# test_conectar()
